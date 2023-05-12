3e2063a7c5dde8447666da4e566cfc96f7953782
chandachewe10 <chewec03@gmail.com>
2023-05-12 01:32:33 +0100

    Fixed Issue on redirect after signing of signatures

--- config/sign-pad.php
+++ config/sign-pad.php
@@ -20,7 +20,7 @@ return [
     /**
      * Route name where you want to redirect users after signing.
      */
-    'redirect_route_name' => 'personaldetails_blade',
+    'redirect_route_name' => 'dashboard',
 
     /**
      * Width and height of the signature rectangle.
